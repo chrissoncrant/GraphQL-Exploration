c30cf076f99a42d180199573cc05ec468c54c5da
Chris Soncrant <christopher.soncrant@gmail.com>
2022-04-12 12:04:56 -0700

    created deeper schema and root value

--- server.js
+++ server.js
@@ -4,14 +4,69 @@ const { graphqlHTTP } = require('express-graphql');
 
 const schema = buildSchema(`
     type Query {
-        description: String
-        price: Float
+        products: [Product]
+        orders: [Order]
+    }
+
+    type Product {
+        id: ID!
+        description: String!
+        price: Float!
+        reviews: [Review]
+    }
+
+    type Review {
+        rating: Int!
+        comment: String
+    }
+
+    type Order {
+        date: String!
+        subtotal: Float!
+        items: [OrderItem] 
+    }
+
+    type OrderItem {
+        product: Product!
+        quantity: Int!
     }
 `);
 
 const root = {
-    description: 'Red Shoe',
-    price: 42.12,
+    products: [
+        {
+            id: 'redshoes',
+            description: 'Red Shoes',
+            price: 42.12,
+            reviews: [
+                {
+                    rating: 5,
+                    comment: 'Loved the shoes!'
+                },
+            ]
+        },
+        {
+            id: 'bluejean',
+            description: 'Blue Jeans',
+            price: 55.55
+        },
+    ],
+    orders: [
+        {
+            date: '2005-05-05',
+            subtotal: 90.22,
+            items: [
+                {
+                    product: {
+                        id: 'redshoes',
+                        description: 'Old Red Shoes',
+                        price: 35.15
+                    },
+                    quantity: 1,
+                },
+            ],
+        }
+    ]
 }
 
 PORT = process.env.PORT || 3000;
